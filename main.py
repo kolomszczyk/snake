fb34dcff8d89c3400655b372c2fd95513a121891
kolomszczyk <kolomszczyk@gmail.com>
2022-05-07 14:22:35 +0200

    formated main.py file

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import pygame
 from time import sleep
 import time
-from snake import * 
+from snake import *
 from utils import *
 
 
 # g stands for grass
 # b stands for body
-# h stands for head of snake 
-# a stands for apple 
+# h stands for head of snake
+# a stands for apple
 
 matrix = [["g" for _ in range(MATRIX_HEIGHT)] for _ in range(MATRIX_WIDTH)]
 
 # Define the colors we will use in RGB format
-BLACK = (  0,   0,   0)
+BLACK = (0,   0,   0)
 WHITE = (255, 255, 255)
-BLUE =  (  0,   0, 255)
-GREEN = (  0, 255,   0)
-RED =   (255,   0,   0)
-
-
+BLUE = (0,   0, 255)
+GREEN = (0, 255,   0)
+RED = (255,   0,   0)
 
 
 def draw(screen: pygame.surface.Surface, snake: Snake):
@@ -29,10 +27,10 @@ def draw(screen: pygame.surface.Surface, snake: Snake):
     for width in range(MATRIX_WIDTH):
         for height in range(MATRIX_HEIGHT):
             # get color of cell
-            
-            # default color 
+
+            # default color
             color = BLACK
-            # body 
+            # body
             if matrix[width][height] == "b":
                 color = GREEN
             # head of snake
@@ -41,14 +39,11 @@ def draw(screen: pygame.surface.Surface, snake: Snake):
             # apple
             if matrix[width][height] == "a":
                 color = RED
-                
 
-            # draw this cell 
+            # draw this cell
             draw_rect(screen, color, width, height)
 
-
-
-    # apple        
+    # apple
     draw_rect(screen, RED, snake.apple_x, snake.apple_y)
 
     # draw snake
@@ -57,16 +52,12 @@ def draw(screen: pygame.surface.Surface, snake: Snake):
     # body
     for point in snake.body:
         draw_rect(screen, GREEN, point[0], point[1])
-    
 
-
-
-    # refresh 
+    # refresh
     pygame.display.flip()
-    
+
 
 def tick(snake: Snake) -> bool:
-
 
     stop_game = snake.is_next_move_collision()
 
@@ -74,13 +65,10 @@ def tick(snake: Snake) -> bool:
     # dont draw last frame
     if stop_game == False:
         return stop_game
-        
+
     snake.move(snake.direction)
 
-
-
     return stop_game
-
 
 
 def main():
@@ -93,14 +81,11 @@ def main():
     snake: Snake = Snake(15, 15)
 
     running = True
-   
+
     t0 = time.time()
 
     # main loop
     while(running):
-        
-         
-        
 
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
@@ -109,7 +94,7 @@ def main():
                 running = False
 
             if event.type == pygame.KEYDOWN:
-            # set snake direction
+                # set snake direction
                 if event.key == pygame.K_a:
                     snake.set_direction("left")
                 if event.key == pygame.K_d:
@@ -119,22 +104,18 @@ def main():
                 if event.key == pygame.K_s:
                     snake.set_direction("down")
 
-
         # run tick fucntion
-        # every 1 s 
-        t1 = time.time()        
+        # every 1 s
+        t1 = time.time()
         if(t1 - t0 >= 0.5):
             # reset clock
             t0 = t1
             running = tick(snake)
 
-        
         draw(screen, snake)
 
     game_end()
 
+
 if __name__ == "__main__":
     main()
-
-
-
